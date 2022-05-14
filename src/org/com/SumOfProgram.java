d1cc1660202dd0011296e79d3e9476a3f9a79b1e
kalaiarasiram <praghyakalai@gmail.com>
2022-05-14 18:19:51 +0530

    message

---
+++ src/org/com/SumOfProgram.java
@@ -0,0 +1,16 @@
+package org.com;
+
+public class SumOfProgram {
+	
+	public SumOfProgram() {
+		
+		System.out.println("Sum");
+
+	}
+	private void sub() {
+		System.out.println("sub");
+ 
+		
+	}
+
+}
